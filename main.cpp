651f57dc39445b886598113c5162b6d3ebe73fb8
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-11-29 10:25:11 +0200

    base code setup

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+
+bool gameOver;
+
+void Setup()
+{
+    
+}
+void Draw()
+{
+    
+}
+void Input()
+{
+    
+}
+void Logic()
+{
+    
+}
+int main()
+{
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+    }
+    return 0;
+}
