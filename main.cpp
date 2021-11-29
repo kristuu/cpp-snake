003a0d2fe117a7ca6b437954f3ee758f58eceba8
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-11-29 10:32:27 +0200

    base variables and Initialize() function setup

--- main.cpp
+++ main.cpp
@@ -4,11 +4,25 @@
 using namespace std;
 
 bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirecton {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirecton dir;
 
-void Setup()
+void Initialize()
 {
-    
+    gameOver = false;
+    dir = STOP; // snake is "frozen"
+    x = width / 2;
+    y = height / 2; // places the snake in the middle of the field
+    fruitX = rand() % width;
+    fruitY = rand() % height; // places the fruit randomly within the field
+    score = 0;
 }
+
 void Draw()
 {
     
@@ -23,7 +37,7 @@ void Logic()
 }
 int main()
 {
-    Setup();
+    Initialize();
     while (!gameOver)
     {
         Draw();
